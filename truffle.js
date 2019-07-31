9c85a5801ca637372a99cbc66735a535c9199527
Chip <G:\Program Files\The Bat!\Mail>
2019-07-31 10:42:00 +0300

    sample MultiSigWallet

---
+++ truffle.js
@@ -0,0 +1,19 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 4000000,
+      gasPrice: 10000000000, // 10 gwei
+    }
+  },
+
+
+compilers: {
+    solc: {
+      version: "^0.4.15", // A version or constraint - Ex. "^0.5.0"   // Can also be set to "native" to use a native solc
+    }
+  }
+
+};
